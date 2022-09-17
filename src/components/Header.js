a273c647b114442c07011f9d47532a53dc77e422
Shubham Sarda <shubhamsarda99@yahoo.com>
2022-09-17 07:00:32 +0530

    Updated Name

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ export const Header = () => {
         <div className="flex flex-wrap justify-between items-center mx-auto">
           <Link to="/" className="flex items-center">
               <img src={Logo} className="mr-2 h-8 sm:h-9" alt="Cinemate Logo" />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinemate</span>
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Cinema Mate</span>
           </Link>
 
           <div id="mobile-nav" className="flex md:order-2">
